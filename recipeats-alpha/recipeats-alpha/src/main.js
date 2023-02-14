8e54af34649bb899bb0b92623b4524e3a7d2e42f
Ivan <ivansam382@gmail.com>
2023-02-14 19:25:11 +0100

    basic crud functionality

--- recipeats-alpha/recipeats-alpha/src/main.js
+++ recipeats-alpha/recipeats-alpha/src/main.js
@@ -54,6 +54,11 @@ function main() {
       const naslovRecepta = document.createElement("h2");
       const obrisiRecept = document.createElement("button");
       const urediRecept = document.createElement("button");
+      const dialogRecept = document.createElement("dialog");
+      const prNaslovForm = document.createElement("form");
+      const promjeniNaslov = document.createElement("input");
+      const promjeniNaslovSubmit = document.createElement("button");
+      const promjeniSadrzaj = document.createElement("textarea");
       //
       jedanRecept.setAttribute("id", recept.id);
       urediRecept.classList.add("urediRecept");
@@ -62,17 +67,33 @@ function main() {
       naslovRecepta.textContent = recept.title;
       const tekstRecepta = document.createElement("p");
       naslovRecepta.classList.add("tekstRecepta");
+      promjeniNaslov.value = recept.title;
+      promjeniSadrzaj.value = recept.instructions;
+      promjeniNaslovSubmit.textContent = "Change Recipe";
+      promjeniNaslovSubmit.setAttribute("type", "submit");
+      promjeniSadrzaj.setAttribute("cols", "30");
+      promjeniSadrzaj.setAttribute("rows", "10");
+      promjeniNaslov.setAttribute("name", "title");
+      promjeniSadrzaj.setAttribute("name", "instructions");
       tekstRecepta.textContent = recept.instructions;
       obrisiRecept.textContent = "X";
       urediRecept.textContent = "Edit";
       obrisiRecept.addEventListener("click", deleteThisRecipe);
       urediRecept.addEventListener("click", editThisRecipe);
       //
+      prNaslovForm.append(
+        promjeniNaslov,
+        promjeniSadrzaj,
+        promjeniNaslovSubmit
+      );
+      dialogRecept.append(prNaslovForm);
+      //
       jedanRecept.append(
         naslovRecepta,
         urediRecept,
         obrisiRecept,
-        tekstRecepta
+        tekstRecepta,
+        dialogRecept
       );
       allRecipes.append(jedanRecept);
     });
@@ -89,7 +110,27 @@ function main() {
   }
 
   function editThisRecipe(element) {
-    console.log(element);
+    const dialog =
+      element.target.nextElementSibling.nextElementSibling.nextElementSibling;
+    dialog.showModal();
+    const changeElement = dialog.lastElementChild.lastElementChild;
+    const changeForm = dialog.lastElementChild;
+    changeForm.addEventListener("submit", postElement);
+    async function postElement(e) {
+      e.preventDefault();
+      const noviRecept = new FormData(changeForm);
+      const posalji = await fetch(
+        `http://localhost:3000/api/recipes/${element.target.parentElement.id}`,
+        {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(Object.fromEntries(noviRecept)),
+        }
+      );
+      const poslano = await posalji.json();
+      dialog.close();
+      location.reload();
+    }
   }
 }
 
